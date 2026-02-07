3942a03855734d3b0f0a2e0bb809323b4a8c0492
shankarx415 <shankarlaptop0506@gmail.com>
2026-02-07 14:37:09 +0530

    Fix: Configure sitemap plugin output directory

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         react(),
         Sitemap({
             hostname: 'https://hexhive.solutions',
+            outDir: 'build',
             dynamicRoutes: [
                 '/',
                 '/programs',
